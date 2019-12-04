e3b3bd519d67385bc2fc6a7b4a3b96691e56525e
MantasBalbierius <mantas.balbierius@gmail.com>
2019-12-04 09:02:05 +0200

    Nodes are using this deprecated parameter.

--- src/provider.ts
+++ src/provider.ts
@@ -1,44 +1,42 @@
-/////
-// Deprecated. All nodes now are public.
-/////
+// Nodes are using this deprecated parameter.
 
-// import { Node } from "./contracts";
+import { Node } from "./contracts";
 
-// type ProvidersList = {
-//     [name: string]: ProviderData;
-// };
+type ProvidersList = {
+    [name: string]: ProviderData;
+};
 
-// interface ProviderData {
-//     isPrivate: boolean;
-// }
+interface ProviderData {
+    isPrivate: boolean;
+}
 
-// const providersList: ProvidersList = {
-//     "noia.network": {
-//         isPrivate: true
-//     }
-// };
+const providersList: ProvidersList = {
+    "noia.network": {
+        isPrivate: true
+    }
+};
 
-// class Provider {
-//     public integrity(providerName: string, node: Node): boolean {
-//         if (!this.check(providerName)) {
-//             return false;
-//         }
+class Provider {
+    public integrity(providerName: string, node: Node): boolean {
+        if (!this.check(providerName)) {
+            return false;
+        }
 
-//         for (const prop in providersList[providerName]) {
-//             if (
-//                 providersList[providerName].hasOwnProperty(prop) &&
-//                 node[prop as keyof Node] !== providersList[providerName][prop as keyof ProviderData]
-//             ) {
-//                 return true;
-//             }
-//         }
+        for (const prop in providersList[providerName]) {
+            if (
+                providersList[providerName].hasOwnProperty(prop) &&
+                node[prop as keyof Node] !== providersList[providerName][prop as keyof ProviderData]
+            ) {
+                return true;
+            }
+        }
 
-//         return false;
-//     }
+        return false;
+    }
 
-//     private check(providerName: string): boolean {
-//         return providersList[providerName] == null;
-//     }
-// }
+    private check(providerName: string): boolean {
+        return providersList[providerName] == null;
+    }
+}
 
-// export let provider = new Provider();
+export let provider = new Provider();
